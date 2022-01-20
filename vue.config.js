f4b12f7ab89d6c02346937047c50eeff180f4d50
dembicki <nikeid1235@gmail.com>
2022-01-20 12:51:43 +0100

    Changed project structure

--- vue.config.js
+++ vue.config.js
@@ -1,3 +1,12 @@
 module.exports = {
   lintOnSave: false,
+  filenameHashing: false,
+  pages: {
+    main: {
+      entry: `${__dirname}/src/modules/main-module/main.js`,
+      template: `${__dirname}/public/main.html`,
+      filename: "index.html",
+      title: "Demo - Greeting",
+    },
+  },
 };
